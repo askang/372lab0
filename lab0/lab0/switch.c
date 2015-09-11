189cc3af89fa5e36128ace29d972ce3f68680f96
askang <askang@email.arizona.edu>
2015-09-11 13:42:42 -0700

    Adding actual files.

---
+++ lab0/lab0/switch.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   switch.c
+ * Author: gvanhoy
+ *
+ * Created on August 27, 2015, 3:12 PM
+ */
+
+#include <xc.h>
+
+#define OUTPUT 0
+#define INPUT 1
+
+void initSwitch1(){
+    TRISDbits.TRISD6 = 1;
+}
