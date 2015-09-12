1f61a2d7bf386ab331f03bb41e4e1e948e600cbd
askang <askang@email.arizona.edu>
2015-09-11 17:23:57 -0700

    Update during office hours Sept 11

--- lab0/lab0/timer.c
+++ lab0/lab0/timer.c
@@ -6,14 +6,27 @@
  */
 
 #include <xc.h>
+#define time 1.0
+#define freq 625,000
+
 
 void initTimer1(){
     //TODO: Initialize Timer 1 to have a period of
     // 1 second. Enable its interrupt
+    TMR1 = 0; //initializes the timer, clears it
+    PR1 = ((freq)*(time)/256)-1; //initialize PR
+    T1CONbits.TCS = 0; //setting the oscillator
+    T1CONbits.TCKPS = 4; //Initializes prescalar to 256
+    IEC0bits.T1IE = 1; //enable the interrupt
+    IFS0bits.T1IF = 0; //puts the flag down
+    IPC1bits.T1IP = 3; //configure Interrupt Priority
+    T1CONbits.ON = 1; //turns on timer
 }
 
 initTimer2(){
     //TODO: Initialize Timer 2.
+    TMR2 = 0; //initializes timer 2
+    
 }
 
 delayMs(int delay){
